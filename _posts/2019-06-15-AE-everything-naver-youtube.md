a623cc841f42d237236c7983b38bbd56ddf31fb6
jaesungbae <bjs2279@gmail.com>
2020-06-21 11:11:36 +0900

    clean

--- _posts/2019-06-15-AE-everything-naver-youtube.md
+++
@@ -1,7 +0,0 @@
----
-layout: post
-title: "오토 인코더의 모든 것"
-subtitle: 이활석님 (NAVER)
-tags: [AE, VAE]
----
-[youtube link](https://www.youtube.com/watch?v=rNh2CrTFpm4)
--- mos/sctts/demo2/tmp.py
+++
@@ -1,7 +0,0 @@
-import glob,os
-wavs = glob.glob('*.wav')
-wavs = sorted(wavs)
-print(wavs)
-for idx, wav in enumerate(wavs):
-    cmd = 'mv {} {}'.format(wav, str(idx) + '.wav')
-    os.system(cmd)
